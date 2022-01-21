ac2a9ea2ebc2df34e23e5ed637de65bba3e12ae6
Sean Claflin <sean_claflin@protonmail.com>
2022-01-21 18:42:31 +0000

    Replaced deprecated module name

--- src/ui/ConfigGroup.js
+++ src/ui/ConfigGroup.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html, css } from 'lit';
 import { baseStyle } from './styles';
 import { infoSquare } from '../lib/images';
 import ConfigItem from './ConfigItem';
